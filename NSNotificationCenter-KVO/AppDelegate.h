fff7974b6396e1c9d8ce5ccb6e04e02ee17a650b
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-17 18:24:08 -0400

    first commit

---
+++ NSNotificationCenter-KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSNotificationCenter-KVO
+//
+//  Created by Jaison Bhatti on 2017-09-17.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
