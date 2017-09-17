c94968d7d7061271969ae86993deaa8407b10142
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-17 19:01:47 -0400

    add count label property in secondVC

--- NSNotificationCenter-KVO/NSNotificationCenter-KVO/FirstViewController.m
+++ NSNotificationCenter-KVO/NSNotificationCenter-KVO/FirstViewController.m
@@ -15,16 +15,6 @@
 
 @implementation FirstViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 - (IBAction)stepperTapped:(UIStepper *)sender {
 }
